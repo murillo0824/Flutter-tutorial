1d8fa5475c8444075abfd93615969404a35b3139
Shoiti Tsuchiya <murillo.mild@gmail.com>
2022-01-01 21:57:50 +0900

    make button section and text section

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // put the theme primary color to the "color"
+    Color color = Theme.of(context).primaryColor;
+    // make the buttonSection
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Call'),
+        _buildButtonColumn(color, Icons.near_me, 'Route'),
+        _buildButtonColumn(color, Icons.share, 'Share'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    // Widget for title section
     Widget titleSection = Container(
         padding: const EdgeInsets.all(32),
         child: Row(
           children: [
             Expanded(
+              /*
+              Putting a Column inside an Expanded widget stretches the column to use all remaining free space in the row. Setting the crossAxisAlignment property to CrossAxisAlignment.start positions the column at the start of the row.
+               */
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  // padding を使用するために最初のカラムをcontainer に格納している
                   Container(
                     padding: const EdgeInsets.only(bottom: 8),
                     child: const Text(
@@ -44,14 +74,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('flutter layout demo'),
-          ),
-          body: Column(
-            children: [
-              titleSection,
-            ],
-          )),
+        appBar: AppBar(
+          title: const Text('flutter layout demo'),
+        ),
+        body: Column(
+          children: [titleSection, buttonSection, textSection],
+        ),
+      ),
+    );
+  }
+
+  /* 
+  button row のパーツは　プライマリーカラーの”アイコン”と”ラベル”で３つとも共通であるので、
+  ヘルパーメソッドを作成して、共通部品を生成しやすくする。
+  いろやサイズの共通部をあらかじめ設定して、変更部分を引数で入れるSCSSのmixinのような感じ
+   */
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              )),
+        ),
+      ],
     );
   }
 }
