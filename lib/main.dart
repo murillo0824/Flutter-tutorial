54023383739c66c495ca22d4e23489f492204051
Shoiti Tsuchiya <murillo.mild@gmail.com>
2022-01-10 11:24:54 +0900

    add assets home.jpg and change the asset settings to directory

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,169 @@ class _TapboxAState extends State<TapBoxA> {
 //---------------------------------------------------------------------
 
 // use parent widget to manage the state
+// making the widget with parent widget manage state
+// "tap box b"
+// ↓
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({Key? key}) : super(key: key);
+
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+// tap box b 子要素
+class TapboxB extends StatelessWidget {
+  const TapboxB({
+    Key? key,
+    this.active = false,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
+}
 
 // =================================================
+
+// mixed match stateful Widget patern
+//depending the widget there are time that this managiment will be the best
+class ParentWidgetC extends StatefulWidget {
+  const ParentWidgetC({Key? key}) : super(key: key);
+
+  @override
+  _ParentWidgetCstate createState() => _ParentWidgetCstate();
+}
+
+class _ParentWidgetCstate extends State<ParentWidgetC> {
+  bool _active = false;
+  void _handleTapboxC(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapboxC(
+        active: _active,
+        onChanged: _handleTapboxC,
+      ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  const TapboxC({
+    Key? key,
+    this.active = false,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: const TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700]!,
+                  width: 10.0,
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+//===========================================
 class FavoriteWidget extends StatefulWidget {
   const FavoriteWidget({Key? key}) : super(key: key);
 
@@ -181,6 +342,14 @@ class MyApp extends StatelessWidget {
             buttonSection,
             textSection,
             const TapBoxA(),
+            const ParentWidget(),
+            const ParentWidgetC(),
+            Image.asset(
+              'images/house.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
           ],
         ),
       ),
